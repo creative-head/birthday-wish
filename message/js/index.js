638b31603ec0fc6c739a54338a2b7c3ba2fe5c24
creative-head <83810866+creative-head@users.noreply.github.com>
2021-05-07 14:21:52 +0530

    Add files via upload

---
+++ message/js/index.js
@@ -0,0 +1,22 @@
+this.$dom = {};
+this.$dom.animatedText = document.createElement('div');
+this.text = "To make your birthday more memorable, I have made something for you.";  // MESSAGE AREA , THIS CAN BE CHANGED FROM YOUR MESSAGE :)
+this.$dom.container = document.getElementById('container');
+this.$dom.container.appendChild(this.$dom.animatedText);
+animateText();
+function animateText(){
+	var arrayOfLetters = this.text.split("");
+	var animatedSpan = [];
+	arrayOfLetters.forEach(function(item){
+		var span = document.createElement('span');
+		span.innerHTML = item;
+		animatedSpan.push(span);
+		span.style.opacity = 0;
+		span.style.color = 'white';
+		this.$dom.animatedText.appendChild(span);
+	});
+	TweenMax.staggerTo(animatedSpan, .2,{
+		opacity : 1,
+		delay : 0.5
+	},0.05);
+}
